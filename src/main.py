cf820d409d84c07d1dd6e0a922c431e4cb03036e
nishizawatakamasa <nszw0502@gmail.com>
2025-03-15 00:44:09 +0900

    処理改善

--- src/main.py
+++ src/main.py
@@ -1,12 +1,30 @@
 import joblib
 import numpy as np
 from fastapi import FastAPI
+from pydantic import BaseModel, field_validator
+from sklearn.linear_model import LinearRegression
 
 app = FastAPI()
 
-model = joblib.load('models/model.joblib')
+model: LinearRegression = joblib.load('models/model.joblib')
 
-@app.get('/predict/')
-def predict(value: float):
-    prediction = model.predict(np.array([[value]]))
-    return {'input': value, 'prediction': prediction[0].item()}
+class InputData(BaseModel):
+    value: float
+
+    @field_validator('value', mode='before')
+    @classmethod
+    def value_must_be_positive(cls, value):
+        if value <= 0:
+            raise ValueError('Value must be positive')
+        return value
+
+class PredictionResult(BaseModel):
+    input: float
+    prediction: float
+
+@app.post('/predict/', response_model=PredictionResult)
+def predict(data: InputData) -> PredictionResult:
+    '''This endpoint takes a single value as input and returns the model's prediction.'''
+    input_value = data.value
+    prediction = model.predict(np.array([[input_value]]))[0]
+    return PredictionResult(input=input_value, prediction=prediction)
